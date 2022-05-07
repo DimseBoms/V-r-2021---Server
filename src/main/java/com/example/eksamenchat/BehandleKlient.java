8786b49ffecab110f5c403cb342ee30374c02ae3
dmitriy <dimseboms@gmail.com>
2022-05-07 14:20:16 +0200

    Første suksessfulle toveiskommunikasjon

--- src/main/java/com/example/eksamenchat/BehandleKlient.java
+++ src/main/java/com/example/eksamenchat/BehandleKlient.java
@@ -3,7 +3,6 @@ package com.example.eksamenchat;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -27,18 +26,37 @@ public class BehandleKlient implements Runnable {
     @Override
     public void run() {
         try {
+            System.out.println("Startet run i BehandleKlient");
             // Etablerer datastrøm for lesing fra klient og skriving til klient
             innStrøm = new ObjectInputStream(socket.getInputStream());
             utStrøm = new ObjectOutputStream(socket.getOutputStream());
+            // Sjekker type forespørsel
+            try {
+                Map input = (Map) innStrøm.readObject();
+                System.out.println(input);
+                System.out.println("Starter lesing av forespørsel");
+                if (input.get("query").equals("sjekkInnBruker")) {
+                    System.out.println("Kontroll av query LIK sjekkInnBruker");
+                    ArrayList<String> romliste = new ArrayList<>();
+                    romliste.add("Testrom1");
+                    romliste.add("Testrom2");
+                    Map<Object, Object> svar = new HashMap<>();
+                    svar.put("status", 1);
+                    svar.put("romliste", romliste);
+                    utStrøm.writeObject(svar);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
             // Leser initiell input når bruker logges inn.
-            Map input = (Map) innStrøm.readObject();
-            if (input != null) {
-                this.tid = new Date(System.currentTimeMillis());
-                this.navn = (String) input.get("brukernavn");
-                this.ip = socket.getInetAddress().getHostAddress();
-                this.rom = (String) input.get("rom");
-                this.melding = (String) input.get("melding");
-                System.out.println("Data inn "+navn);
+            //Map input = (Map) innStrøm.readObject();
+            //if (input != null) {
+            //    this.tid = new Date(System.currentTimeMillis());
+            //    this.navn = (String) input.get("brukernavn");
+            //    this.ip = socket.getInetAddress().getHostAddress();
+            //    this.rom = (String) input.get("rom");
+            //    this.melding = (String) input.get("melding");
+            //   System.out.println("Data inn "+navn);
                 // Skal feste kall på adaptor/database her
 
                 // Alle aktuelle kall på Adaptor:
@@ -46,18 +64,18 @@ public class BehandleKlient implements Runnable {
                 //     Adaptor.insertLogg(tid, navn, ip, rom, null);
                 //     Adaptor.insertLogg(tid, navn, ip, rom, melding);
 
-                Adaptor.insertLogg(tid,navn, ip, rom, melding);
+            //    Adaptor.insertLogg(tid,navn, ip, rom, melding);
 
             }
-            ArrayList<String> romliste = new ArrayList<>();
-            romliste.add("TestRom1");
-            HashMap<Object, Object> svar = new HashMap<>();
-            svar.put("status", 1);
-            svar.put("romliste", romliste);
-            utStrøm.writeObject(svar);
-            innStrøm.close();
-            utStrøm.close();
-        } catch (IOException | ClassNotFoundException e) {
+        //ArrayList<String> romliste = new ArrayList<>();
+        ////  romliste.add("TestRom1");
+        //HashMap<Object, Object> svar = new HashMap<>();
+        //  svar.put("status", 1);
+        //  svar.put("romliste", romliste);
+        //  utStrøm.writeObject(svar);
+        //  innStrøm.close();
+        //  utStrøm.close();
+        } catch (IOException e) {
             System.out.println("FEIL "+e.getMessage());
           //  System.out.println();
         }
